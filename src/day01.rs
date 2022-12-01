2f2ee653430408ddc55dec5a62c163859e943a9a
Sev <sevm@pm.me>
2022-12-02 00:04:54 +0100

    typo lul

--- src/day01.rs
+++ src/day01.rs
@@ -13,28 +13,28 @@ fn parse_input(input: &str) -> Vec<u32> {
         })
         .collect::<Vec<_>>();
 
-    let mut elfes = vec![];
+    let mut elves = vec![];
     let mut current_elf = 0;
     for input in inputs {
         match input {
             None => {
-                elfes.push(current_elf);
+                elves.push(current_elf);
                 current_elf = 0;
             }
             Some(value) => current_elf += value,
         };
     }
 
-    elfes
+    elves
 }
 
 #[aoc(day1, part1)]
-fn part1(elfes: &[u32]) -> u32 { *elfes.iter().max().unwrap() }
+fn part1(elves: &[u32]) -> u32 { *elves.iter().max().unwrap() }
 
 #[aoc(day1, part2)]
-fn part2(elfes: &[u32]) -> u32 {
-    let mut elfes = elfes.to_vec();
+fn part2(elves: &[u32]) -> u32 {
+    let mut elves = elves.to_vec();
 
-    elfes.sort();
-    elfes.into_iter().rev().take(3).sum::<u32>()
+    elves.sort();
+    elves.into_iter().rev().take(3).sum::<u32>()
 }
