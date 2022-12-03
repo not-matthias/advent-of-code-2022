44c619a68a75506d684bfd5656bb051f14a612c8
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-12-03 10:42:59 +0100

    feat: add part 1 solution

---
+++ src/day03.rs
@@ -0,0 +1,109 @@
+use std::collections::HashSet;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Item(char);
+
+impl Item {
+    /// Returns the following:
+    /// 1 = a, 26 = z
+    /// 27 = A, 52 = Z
+    pub fn priority(&self) -> u8 {
+        match self.0 {
+            'a'..='z' => self.0 as u8 - 96,
+            'A'..='Z' => self.0 as u8 - 64 + 26,
+            _ => 0,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Rucksack {
+    pub items: Vec<Item>,
+}
+
+impl Rucksack {
+    pub fn new(items: Vec<Item>) -> Self { Self { items } }
+
+    pub fn shared_items(&self) -> HashSet<Item> {
+        // Panic if we have an uneven number of items
+        assert_eq!(self.items.len() % 2, 0);
+
+        let (part1, part2) = self.items.split_at(self.items.len() / 2);
+
+        let mut shared = HashSet::new();
+        for item in part2 {
+            if part1.contains(item) {
+                shared.insert(*item);
+            }
+        }
+
+        shared
+    }
+
+    /// Checks if the item is in both compartments.
+    pub fn contains_item(&self, item: Item) -> bool {
+        let (part1, part2) = self.items.split_at(self.items.len() / 2);
+
+        part1.contains(&item) && part2.contains(&item)
+    }
+}
+
+#[aoc_generator(day3)]
+fn parse_input(input: &str) -> Vec<Rucksack> {
+    input
+        .lines()
+        .map(|line| {
+            Rucksack::new(line.chars().map(|c| Item(c)).collect::<Vec<Item>>())
+        })
+        .collect::<Vec<_>>()
+}
+
+#[aoc(day3, part1)]
+fn solve_part_1(input: &[Rucksack]) -> u32 {
+    let shared = input
+        .iter()
+        .map(|r| r.shared_items())
+        .flatten()
+        .collect::<Vec<_>>();
+
+    shared.into_iter().map(|i| i.priority() as u32).sum()
+}
+
+#[aoc(day3, part2)]
+fn solve_part_2(_input: &[Rucksack]) -> u32 {
+    //
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_priority() {
+        assert_eq!(Item('a').priority(), 1);
+        assert_eq!(Item('z').priority(), 26);
+        assert_eq!(Item('A').priority(), 27);
+        assert_eq!(Item('Z').priority(), 52);
+    }
+
+    fn get_input() -> &'static str {
+        r#"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"#
+    }
+
+    #[test]
+    fn test_example() {
+        let input = get_input();
+        let input = parse_input(input);
+
+        assert_eq!(solve_part_1(&input), 157);
+        // assert_eq!(solve_part_2(&input), 0);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 pub mod day01;
 pub mod day02;
+pub mod day03;
 
 #[cfg(target_env = "DONT_COMPILE_THIS")] pub mod template;
 
