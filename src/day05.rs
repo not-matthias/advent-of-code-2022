a313108da4c4262e4275351b20bbca54559171a3
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-12-05 20:53:53 +0100

    feat: add part1 sol

---
+++ src/day05.rs
@@ -0,0 +1,115 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::{collections::VecDeque, str::FromStr};
+
+#[derive(Clone, Debug)]
+pub struct Move {
+    from: usize,
+    to: usize,
+    count: usize,
+}
+
+#[derive(Clone, Debug)]
+pub struct Ship {
+    crates: Vec<VecDeque<char>>,
+    moves: Vec<Move>,
+}
+
+impl FromStr for Ship {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut parts = input.split("\n\n");
+
+        // Parse the stack
+        //
+        let lines = parts.next().unwrap().lines();
+        let stack_count = (input.lines().next().unwrap().len() + 1) / 4;
+        let mut crates = vec![VecDeque::new(); stack_count];
+
+        for line in lines {
+            for i in 0..stack_count {
+                if line.chars().nth(i * 4 + 0) == Some('[') {
+                    crates[i].push_front(line.chars().nth(i * 4 + 1).unwrap());
+                }
+            }
+        }
+
+        // Parse the moves
+        //
+        let mut moves = Vec::new();
+        for line in parts.next().unwrap().lines() {
+            lazy_static! {
+                static ref RE: Regex =
+                    Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+            }
+
+            let captures = RE.captures(line).unwrap();
+            moves.push(Move {
+                from: captures[2].parse().unwrap(),
+                to: captures[3].parse().unwrap(),
+                count: captures[1].parse().unwrap(),
+            });
+        }
+
+        Ok(Ship { crates, moves })
+    }
+}
+
+#[aoc_generator(day5)]
+fn parse_input(input: &str) -> Ship { input.parse().unwrap() }
+
+#[aoc(day5, part1)]
+fn solve_part_1(input: &Ship) -> String {
+    let mut input: Ship = input.clone();
+
+    for mv in &input.moves {
+        for _ in 0..mv.count {
+            if let Some(item) = input.crates[mv.from - 1].pop_back() {
+                input.crates[mv.to - 1].push_back(item);
+            }
+        }
+    }
+
+    let mut result = String::new();
+    for c in &input.crates {
+        if let Some(item) = c.back() {
+            result.push(*item);
+        }
+    }
+
+    result
+}
+
+#[aoc(day5, part2)]
+fn solve_part_2(input: &Ship) -> u32 {
+    //
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_input() -> &'static str {
+        r#"    [D]    
+[N] [C]    
+[Z] [M] [P]
+    1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"#
+    }
+
+    #[test]
+    fn test_example() {
+        let input = get_input();
+        let input = parse_input(input);
+
+        assert_eq!(solve_part_1(&input), "CMZ");
+        // assert_eq!(solve_part_2(&input), 0);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod day01;
 pub mod day02;
 pub mod day03;
 pub mod day04;
+pub mod day05;
 
 #[cfg(target_env = "DONT_COMPILE_THIS")] pub mod template;
 
